183f1b441c2046c5fbc0b74e7a08844d3984e76a
ronit18 <ronitgandhi96@gmail.com>
2023-05-10 23:49:54 +0530

    Changed the directory of page.tsx to (site)/page.tsx

---
+++ app/(site)/page.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import AuthForm from "./components/AuthForm";
+
+export default function Home() {
+	return (
+		<div className="flex min-h-full flex-col justify-center items-center h-screen py-12 sm:px-6 lg:px-8 bg-gray-100">
+			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+				<Image
+					alt="logo"
+					src="/logo.png"
+					width="68"
+					height="68"
+					className="mx-auto w-auto"
+				/>
+				<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+					{" "}
+					Sign in to your account
+				</h2>
+			</div>
+			<AuthForm />
+		</div>
+	);
+}
