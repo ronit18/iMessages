5024a3b579660eb23e8328a1fcdaa35d8dbde8f5
ronit18 <ronitgandhi96@gmail.com>
2023-05-19 18:55:48 +0530

    added sign in button and github and google sign in buttons.

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,21 +1,23 @@
-"use client";
+'use client';
 
-import Button from "@/app/components/Button";
-import Input from "@/app/components/inputs/Input";
-import { useCallback, useState } from "react";
-import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import Button from '@/app/components/Button';
+import Input from '@/app/components/inputs/Input';
+import { useCallback, useState } from 'react';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
-type variant = "LOGIN" | "REGISTER";
+type variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
-	const [variant, setVariant] = useState<variant>("LOGIN");
-	const [Loading, setIsLoading] = useState(false);
+	const [variant, setVariant] = useState<variant>('LOGIN');
+	const [isLoading, setIsLoading] = useState(false);
 
 	const toggleVariant = useCallback(() => {
-		if (variant === "LOGIN") {
-			setVariant("REGISTER");
+		if (variant === 'LOGIN') {
+			setVariant('REGISTER');
 		} else {
-			setVariant("LOGIN");
+			setVariant('LOGIN');
 		}
 	}, [variant]);
 
@@ -25,18 +27,18 @@ const AuthForm = () => {
 		formState: { errors },
 	} = useForm<FieldValues>({
 		defaultValues: {
-			name: "",
-			email: "",
-			password: "",
+			name: '',
+			email: '',
+			password: '',
 		},
 	});
 
 	const onSubmit: SubmitHandler<FieldValues> = (data) => {
 		setIsLoading(true);
-		if (variant === "REGISTER") {
+		if (variant === 'REGISTER') {
 			// axios register
 		}
-		if (variant === "LOGIN") {
+		if (variant === 'LOGIN') {
 			// axios login
 		}
 	};
@@ -49,32 +51,82 @@ const AuthForm = () => {
 	return (
 		<div className=" mt-8 sm:mx-auto sm:w-full sm:max-w-md">
 			<div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+				<h2 className="my-5 text-center text-3xl font-bold tracking-tight text-gray-900">
+					{' '}
+					{variant === 'LOGIN'
+						? 'Sign in to your account'
+						: 'Create a new account'}
+				</h2>
 				<form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-					{variant === "REGISTER" && (
+					{variant === 'REGISTER' && (
 						<Input
 							id="name"
 							label="Name"
 							register={register}
 							errors={errors}
+							disabled={isLoading}
 						/>
 					)}
 					<Input
 						id="email"
 						label="Email address"
 						register={register}
+						disabled={isLoading}
 						errors={errors}
 					/>
 					<Input
 						id="password"
 						label="Password"
 						register={register}
+						disabled={isLoading}
 						errors={errors}
 					/>
 
 					<div>
-						<Button />
+						<Button disabled={isLoading} fullwidth type="submit">
+							{variant === 'LOGIN' ? 'Sign In' : 'Register'}
+						</Button>
 					</div>
 				</form>
+
+				<div className="mt-6">
+					<div className="relative">
+						<div className="absolute inset-0 flex items-center">
+							<div className="w-full border-t border-gray-300" />
+						</div>
+
+						<div className="relative flex justify-center text-sm">
+							<span className="bg-white px-2 text-gray-500">
+								or continue with
+							</span>
+						</div>
+					</div>
+
+					<div className="mt-6 flex gap-2">
+						<AuthSocialButton
+							icons={BsGoogle}
+							onClick={() => socialAction('github')}
+						/>
+						<AuthSocialButton
+							icons={BsGithub}
+							onClick={() => socialAction('google')}
+						/>
+					</div>
+				</div>
+
+				<div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+					<div>
+						{variant === 'LOGIN'
+							? 'New to iMessages?'
+							: 'Already have an account?'}
+					</div>
+					<div
+						onClick={toggleVariant}
+						className="underline cursor-pointer"
+					>
+						{variant === 'LOGIN' ? 'Create an account' : 'Login'}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,4 +1,45 @@
-const Button = () => {
-	return <div>Button</div>;
+'use client';
+import clsx from 'clsx';
+import React from 'react';
+
+interface ButtonProps {
+	type?: 'button' | 'submit' | 'reset' | undefined;
+	fullwidth?: boolean;
+	children?: React.ReactNode;
+	onClick?: () => void;
+	secondry?: boolean;
+	danger?: boolean;
+	disabled?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+	type,
+	fullwidth,
+	children,
+	onClick,
+	secondry,
+	danger,
+	disabled,
+}) => {
+	return (
+		<button
+			onClick={onClick}
+			type={type}
+			disabled={disabled}
+			className={clsx(
+				`flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2`,
+				disabled && 'opacity-50 cursor-default',
+				fullwidth && 'w-full',
+				secondry ? 'text-gray-900' : 'text-white',
+				danger &&
+					'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+				!secondry &&
+					!danger &&
+					'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600 '
+			)}
+		>
+			{children}
+		</button>
+	);
 };
 export default Button;
