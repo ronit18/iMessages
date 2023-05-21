34a1654192f833f13c031525d376a7f447efa53b
ronit18 <ronitgandhi96@gmail.com>
2023-05-21 21:02:55 +0530

    fixed a issue.

--- app/hooks/useConversations.ts
+++ app/hooks/useConversations.ts
@@ -6,9 +6,9 @@ const useConversation = () => {
 	const conversationId = useMemo(() => {
 		if (!params?.conversationId) {
 			return '';
-		} else {
-			return params?.conversationId as string;
 		}
+
+		return params.conversationId as string;
 	}, [params?.conversationId]);
 	const isOpen = useMemo(() => !!conversationId, [conversationId]);
 
