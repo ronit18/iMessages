95ed50ee002fc67fd181408d01f8988ac7c134d0
ronit18 <ronitgandhi96@gmail.com>
2023-05-22 09:45:05 +0530

    fixed a typescript issue.

--- app/(site)/components/AuthSocialButton.tsx
+++ app/(site)/components/AuthSocialButton.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { IconType } from 'react-icons';
 
 interface AuthSocialButtonProps {
-	icons?: IconType;
-	onClick?: () => void;
+	icons: IconType;
+	onClick: () => void;
 }
 
 const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
@@ -14,7 +13,22 @@ const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
 		<button
 			type="button"
 			onClick={onClick}
-			className="inline-flex w-full justify-center rounded-md bg-white px-4 py-2 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:offset-0"
+			className="
+        inline-flex
+        w-full 
+        justify-center 
+        rounded-md 
+        bg-white 
+        px-4 
+        py-2 
+        text-gray-500 
+        shadow-sm 
+        ring-1 
+        ring-inset 
+        ring-gray-300 
+        hover:bg-gray-50 
+        focus:outline-offset-0
+      "
 		>
 			<Icon />
 		</button>
