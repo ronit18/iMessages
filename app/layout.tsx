6ad11415d10c0d49133648a1a99ded96c9741cb8
ronit18 <ronitgandhi96@gmail.com>
2023-06-02 18:43:58 +0530

    Made the functionality for Active status.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import AuthContext from './context/AuthContext';
 import ToasterContext from './context/ToasterContext';
+import ActiveStatus from './components/ActiveStatus';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
 		<html lang="en">
 			<body className={inter.className}>
 				<AuthContext>
+					<ActiveStatus />
 					<ToasterContext />
 					{children}
 				</AuthContext>
--- app/libs/pusher.ts
+++ app/libs/pusher.ts
@@ -11,7 +11,12 @@ export const pusherServer = new PusherServer({
 
 export const pusherClient = new PusherClient(
 	process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
+
 	{
+		channelAuthorization: {
+			endpoint: '/api/pusher/auth',
+			transport: 'ajax',
+		},
 		cluster: 'ap2',
 	}
 );
